2b9606f9d253e25abbe06c642bdb8c78651cbf76
HoweCheung <scantfly@gmail.com>
2022-02-06 09:52:44 +0800

    Add Sub_info_Lite.js

---
+++ Surge/Panel scripts/Sub_info_Lite.js
@@ -0,0 +1,131 @@
+(async () => {
+  let args = getArgs();
+  let info = await getDataInfo(args.url);
+  if (!info) $done();
+  let resetDayLeft = getRmainingDays(parseInt(args["reset_day"]));
+  let used = info.download + info.upload;
+  let total = info.total;
+  let left;
+  if (used >= total) {
+      left = 0
+  } else {
+      left = total - used
+  };
+  console.log(used)
+  console.log(total)
+  console.log(left)
+  let expire = args.expire || info.expire;
+  let content = [`Info: ${bytesToSize(left)} | `];
+
+  if (resetDayLeft) {
+    content.push(`${resetDayLeft}`);
+  }
+  if (expire) {
+    if (/^[\d.]+$/.test(expire)) expire *= 1000;
+    content.push(`${formatTime(expire)}`);
+  }
+
+  let now = new Date();
+  let hour = now.getHours();
+  let minutes = now.getMinutes();
+  hour = hour > 9 ? hour : "0" + hour;
+  minutes = minutes > 9 ? minutes : "0" + minutes;
+
+  $done({
+    title: `${args.title} | ${hour}:${minutes}`,
+    content: content.join(""),
+    icon: args.icon || "airplane.circle",
+    "icon-color": args.color || "#007aff",
+  });
+})();
+
+function getArgs() {
+  return Object.fromEntries(
+    $argument
+      .split("&")
+      .map((item) => item.split("="))
+      .map(([k, v]) => [k, decodeURIComponent(v)])
+  );
+}
+
+function getUserInfo(url) {
+  let request = { headers: { "User-Agent": "Quantumult%20X" }, url };
+  return new Promise((resolve, reject) =>
+    $httpClient.get(request, (err, resp) => {
+      if (err != null) {
+        reject(err);
+        return;
+      }
+      if (resp.status !== 200) {
+        reject(resp.status);
+        return;
+      }
+      let header = Object.keys(resp.headers).find(
+        (key) => key.toLowerCase() === "subscription-userinfo"
+      );
+      if (header) {
+        resolve(resp.headers[header]);
+        return;
+      }
+      reject("链接响应头不带有流量信息");
+    })
+  );
+}
+
+async function getDataInfo(url) {
+  const [err, data] = await getUserInfo(url)
+    .then((data) => [null, data])
+    .catch((err) => [err, null]);
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  return Object.fromEntries(
+    data
+      .match(/\w+=[\d.eE+]+/g)
+      .map((item) => item.split("="))
+      .map(([k, v]) => [k, Number(v)])
+  );
+}
+
+function getRmainingDays(resetDay) {
+  if (!resetDay) return;
+
+  let now = new Date();
+  let today = now.getDate();
+  let month = now.getMonth();
+  let year = now.getFullYear();
+  let daysInMonth;
+  let nextMonth;
+  if (month + 2 > 12) {
+    nextMonth =  month + 2 - 12
+  } else if (month + 2 > 9) {
+    nextMonth =  month + 2
+  } else {
+    nextMonth = month + 2
+    nextMonth = '0' + `${nextMonth}`
+  }
+  if (resetDay > 10) {
+    resetDay = resetDay
+  } else {
+    resetDay = '0' + `${resetDay}`
+  }
+  return `${year}` + '-' + `${nextMonth}` + '-' + `${resetDay}`;
+}
+
+function bytesToSize(bytes) {
+  if (bytes === 0) return "0B";
+  let k = 1024;
+  sizes = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+  let i = Math.floor(Math.log(bytes) / Math.log(k));
+  return (bytes / Math.pow(k, i)).toFixed(2) + " " + sizes[i];
+}
+
+function formatTime(time) {
+  let dateObj = new Date(time);
+  let year = dateObj.getFullYear();
+  let month = dateObj.getMonth() + 1;
+  let day = dateObj.getDate();
+  return year + "年" + month + "月" + day + "日";
+}
