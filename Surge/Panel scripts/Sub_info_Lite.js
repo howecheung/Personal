c16f616f89f6b0629a49ed324d6bd97f01e7dc20
HoweCheung <scantfly@gmail.com>
2022-02-07 13:30:16 +0800

    Update Sub_info_Lite.js

--- Surge/Panel scripts/Sub_info_Lite.js
+++ Surge/Panel scripts/Sub_info_Lite.js
@@ -1,3 +1,30 @@
+/*
+Surge配置参考注释，感谢@asukanana,感谢@congcong.
+
+示例↓↓↓ 
+----------------------------------------
+
+[Script]
+Sub_info = type=generic,timeout=10,script-path=https://raw.githubusercontent.com/mieqq/mieqq/master/sub_info_panel.js,script-update-interval=0,argument=url=[URL encode 后的机场节点链接]&reset_day=1&title=AmyInfo&icon=bonjour&color=#007aff
+
+[Panel]
+Sub_info = script-name=Sub_info,update-interval=600
+
+----------------------------------------
+
+先将带有流量信息的节点订阅链接encode，用encode后的链接替换"url="后面的[机场节点链接]
+
+可选参数 &reset_day，后面的数字替换成流量每月重置的日期，如1号就写1，8号就写8。如"&reset_day=8",不加该参数不显示流量重置信息。
+
+可选参数 &expire，机场链接不带expire信息的，可以手动传入expire参数，如"&expire=2022-02-01",注意一定要按照yyyy-MM-dd的格式。
+
+可选参数"title=xxx" 可以自定义标题。
+
+可选参数"icon=xxx" 可以自定义图标，内容为任意有效的 SF Symbol Name，如 bolt.horizontal.circle.fill，详细可以下载app https://apps.apple.com/cn/app/sf-symbols-browser/id1491161336
+
+可选参数"color=xxx" 当使用 icon 字段时，可传入 color 字段控制图标颜色，字段内容为颜色的 HEX 编码。如：color=#007aff
+----------------------------------------
+*/
 (async () => {
   let args = getArgs();
   let info = await getDataInfo(args.url);
@@ -15,7 +42,7 @@
   console.log(total)
   console.log(left)
   let expire = args.expire || info.expire;
-  let content = [`Info: ${bytesToSize(left)} | `];
+  let content = [`Info: ${bytesToSize(left)}`];
 
   if (resetDayLeft) {
     content.push(`${resetDayLeft}`);
@@ -33,7 +60,7 @@
 
   $done({
     title: `${args.title} | ${hour}:${minutes}`,
-    content: content.join(""),
+    content: content.join(" | "),
     icon: args.icon || "airplane.circle",
     "icon-color": args.color || "#007aff",
   });
@@ -96,10 +123,10 @@ function getRmainingDays(resetDay) {
   let today = now.getDate();
   let month = now.getMonth();
   let year = now.getFullYear();
-  let daysInMonth;
   let nextMonth;
   if (month + 2 > 12) {
     nextMonth =  month + 2 - 12
+    year = year + 1
   } else if (month + 2 > 9) {
     nextMonth =  month + 2
   } else {
